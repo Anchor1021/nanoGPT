e0b8a36196d18b5a7901985186884d0b69aa412f
Anchor1021 <bzhao78@wisc.edu>
2024-05-17 22:47:12 -0700

    typo fixed in GTPConfig

--- model.py
+++ model.py
@@ -121,10 +121,10 @@ class GPTConfig:
     n_embd: int = 768
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
-
     #new params
-    query_size = 64
-    key_size = 64
+    key_size: int = 64
+    query_size: int = 64
+    
 
 class GPT(nn.Module):
 
