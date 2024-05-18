2e06f651d16ff921cdc1b6939dc55b8675a86cad
Anchor1021 <bzhao78@wisc.edu>
2024-05-17 22:13:22 -0700

    add params in GPTConfig

--- model.py
+++ model.py
@@ -122,6 +122,10 @@ class GPTConfig:
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
+    #new params
+    query_size = 64
+    key_size = 64
+
 class GPT(nn.Module):
 
     def __init__(self, config):
